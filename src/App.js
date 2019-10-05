303dd7507931cc1e79b0b2c8875d7373e4732a64
Denis <dmarkitanov@gmail.com>
2019-10-05 17:16:39 +0600

    need to redo a little to make it input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Button from './components/atoms/Button'
 
 function App() {
+
+  const callback = () => {
+    alert('nigga')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Button
+        text='nigger faggot'
+        color='#000000'
+        textColor='#ffffff'
+        width={50}
+        callback={callback}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
